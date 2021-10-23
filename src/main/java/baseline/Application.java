a92e2014d92e5011a3ebf575c5acc3499f1c8c95
WhorTez <dortiz1@knights.ucf.edu>
2021-10-22 22:56:10 -0400

    GUI mockup

---
+++ src/main/java/baseline/Application.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Application {
+    public static void main(String[] args){
+//
+    }
+}
